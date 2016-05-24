84e18dcfeac7b79d3e1bebe147025fadf57d8f0c
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-23 21:46:45 -0500

    Primer Commit

---
+++ ProyectoMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Este es el Método Index()";
+        }
+
+        public string Proceso()
+        {
+            return "Este es el metodo Proceso()";
+        }
+    }
+}
