58bfb7c7a030febf3b0d52d14e6c229ecedd1d71
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-24 20:17:59 -0500

    Agregando Entity Framework

---
+++ clase02/lab01/Lab01/Lab01/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab01.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [VerifyEmployees]
+        public int NumberEmployees { get; set; }
+    }
+
+    public class VerifyEmployees : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int number = (int)value;
+            return number < 20;
+        }
+
+        public VerifyEmployees()
+        {
+            ErrorMessage = "Información no Válida";
+        }
+    }
+
+   
+
+}
---
+++ clase02/lab01/Lab01/Lab01/Models/CustomerDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Lab01.Models
+{
+    public class CustomerDB :DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
