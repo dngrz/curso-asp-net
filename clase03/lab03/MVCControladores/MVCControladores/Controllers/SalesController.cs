6c168163573841e665d2bdb11f509110c1523ff8
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-25 20:30:49 -0500

    Laboratorio 03

---
+++ clase03/lab03/MVCControladores/MVCControladores/Controllers/SalesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControladores.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public string Index()
+        {
+            return "ACCION INDEX SIN PARAMETROS";
+        }
+
+        public string Procesar(string name, string city)
+        {
+            return "PARAMETRO 1: " + name + " PARAMETRO 2: "+ city;
+        }
+
+        public ActionResult Listar(string titulo, int nveces=0)
+        {
+            ViewBag.Titulo = titulo;
+            ViewBag.NumeroVeces = nveces;
+            return View();
+        }
+    }
+}
---
+++ clase03/lab03/MVCControladores/MVCControladores/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCControladores.MvcApplication" Language="C#" %>
