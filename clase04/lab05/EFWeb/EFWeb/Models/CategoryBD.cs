35394af7ddc3497d43e838f1ab0dcbc4dba30751
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-26 21:25:02 -0500

    MVC desde Cero, Creando BD con EntityFRamework

---
+++ clase04/lab05/EFWeb/EFWeb/Models/CategoryBD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EFWeb.Models
+{
+    public class CategoryBD : DbContext
+    {
+        public DbSet<Category> categories { get; set; }
+    }
+}
