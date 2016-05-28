d07da3ac02d34dc154dfdcea4cdb470540083a4a
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-28 13:23:33 -0500

    Creacion de Prueba Unitaria MSTEST

---
+++ clase06/lab09/ProyectoFuente/ProyectoFuente/Ventas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFuente
+{
+    public class Ventas
+    {
+        public double CalcularIGV(double montoFactura)
+        {
+            double resultado;
+            resultado = montoFactura * 0.18;
+            return resultado;
+        }
+    }
+}
---
+++ clase06/lab09/ProyectoFuente/ProyectoFuenteTests/VentasTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFuente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFuente.Tests
+{
+    [TestClass()]
+    public class VentasTests
+    {
+        [TestMethod()]
+        public void CalcularIGVTest()
+        {
+            Ventas ventas = new Ventas();
+            double resultado = ventas.CalcularIGV(100);
+            Assert.AreEqual(18, resultado);
+        }
+    }
+}
