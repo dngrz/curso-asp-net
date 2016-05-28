513f600c54695961c57b7de001c52bfd9033f19f
clodoaldosanchez@gmail.com <clodoaldosanchez@gmail.com>
2016-05-27 20:11:03 -0500

    Continuación Creacion de Boton Crear

---
+++ clase05/lab06/EFWeb/EFWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFWeb.Models;
+using System.Net;
+
+namespace EFWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // Creando la instancia del administrador de entidades locales
+        private CategoryBD db = new CategoryBD();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Category category = db.categories.Find(id);
+            if(category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "categoryID,description,salesContact,salesNumbers")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Categories/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ clase05/lab06/EFWeb/EFWeb/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFWeb.Models
+{
+    public class Category
+    {
+        public int categoryID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string description { get; set; }
+
+        [StringLength(100)]
+        public string salesContact { get; set; }
+
+        [Required]
+        public int salesNumbers { get; set; }
+    }
+}
